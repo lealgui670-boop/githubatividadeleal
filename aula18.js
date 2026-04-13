cbf5a1f0356eeaef14db147c33a0c14291aaf818
lealgui670-boop <lealgui670@gmail.com>
2026-04-13 19:52:41 -0300

    subindo arquivo

---
+++ aula18.js
@@ -0,0 +1,9 @@
+const numeroAleatorio = Math.random();
+// const numeroAleatorio = 0.9999;
+console.log('numeroAleatorio', numeroAleatorio);
+
+const numeroMaximo = 100;
+console.log('numeroMaximo', numeroAleatorioMaximo);
+
+const numeroAleatorioInteiro = Math.floor(numeroAleatorio * numeroMaximo) + 1;
+console.log('numeroAleatorioInteiro', numeroAleatorioInteiro);
